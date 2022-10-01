608e6dc45579f718fdabc42e93396f71d74ea72d
evgenii litvin <ev_litvin@yahoo.com>
2022-10-01 20:30:04 +0500

    Init commit.

---
+++ src/test/java/ru/litvin/ev/virtuallibrary/VirtualLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.litvin.ev.virtuallibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
