268ffa3302dbbe96041a9711f315498e61f97689
evgenii litvin <ev_litvin@yahoo.com>
2022-10-03 12:36:47 +0500

    Add SecurityConfig.

---
+++ src/main/java/ru/litvin/ev/virtuallibrary/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package ru.litvin.ev.virtuallibrary.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeRequests()
+                .antMatchers("/library").access("hasRole('USER')")
+                .antMatchers("/", "/about", "/contacts").access("permitAll()")
+
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/library")
+
+                .and()
+                .csrf()
+                .ignoringAntMatchers("/h2-console/**")
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .build();
+    }
+
+}
