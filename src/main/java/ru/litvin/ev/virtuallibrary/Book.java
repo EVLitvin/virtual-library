a001462cbd1681b8432d25cad7f0392d55718a3c
evgenii litvin <ev_litvin@yahoo.com>
2022-10-05 13:12:05 +0500

    Add Book validation.

--- src/main/java/ru/litvin/ev/virtuallibrary/Book.java
+++ src/main/java/ru/litvin/ev/virtuallibrary/Book.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -19,6 +21,13 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    @NotBlank
+    @Size(min = 2, max = 30, message = "The book title cannot be less than" +
+                                        " 2 characters and more than 30 characters")
     private String title;
+
+    @NotBlank
+    @Size(min = 5, max = 30, message = "The author's name cannot be less than"
+                                        + " 3 characters and more than 30 characters")
     private String author;
 }
--- src/main/java/ru/litvin/ev/virtuallibrary/controllers/LibraryController.java
+++ src/main/java/ru/litvin/ev/virtuallibrary/controllers/LibraryController.java
@@ -2,12 +2,15 @@ package ru.litvin.ev.virtuallibrary.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.litvin.ev.virtuallibrary.Book;
 import ru.litvin.ev.virtuallibrary.data.BookRepository;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/library")
@@ -31,7 +34,11 @@ public class LibraryController {
     }
 
     @PostMapping
-    public String donateBookToLibrary(Book book) {
+    public String donateBookToLibrary(@Valid Book book, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("allBooks", bookRepo.findAll());
+            return "library";
+        }
         bookRepo.save(book);
         return "redirect:/library";
     }
