6c163adb6fccc11ff66d13dc7dc78a26d4aa8baf
evgenii litvin <ev_litvin@yahoo.com>
2022-10-11 14:44:46 +0500

    Delete initial data.

--- src/main/java/ru/litvin/ev/virtuallibrary/VirtualLibraryApplication.java
+++ src/main/java/ru/litvin/ev/virtuallibrary/VirtualLibraryApplication.java
@@ -1,24 +1,13 @@
 package ru.litvin.ev.virtuallibrary;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import ru.litvin.ev.virtuallibrary.data.BookRepository;
 
 @SpringBootApplication
 public class VirtualLibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VirtualLibraryApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner dataLoader(BookRepository bookRepo) {
-        return args -> {
-            bookRepo.save(new Book(1L, "War and Peace", "Leo Tolstoy"));
-            bookRepo.save(new Book(2L, "Capital", "Karl Marks"));
-        };
     }
 
 }
